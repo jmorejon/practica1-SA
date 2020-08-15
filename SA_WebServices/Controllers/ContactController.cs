aab2baff63c1d94852f0484236a1c6cff12163d5
jmorejon <jmorejon@ingenieria.usac.edu.gt>
2020-08-14 23:31:06 -0600

    Agregar nueva solucion Ahora se trabajara con visual studio Asp.Net Core

---
+++ SA_WebServices/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SA_WebServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        public ContactController() { }
+        //GET: api/contact/list
+        [HttpGet]
+        [Route("list")]
+        public string Lista()
+        {
+
+            ServiceReference1.administratorcontact100Client ws = new ServiceReference1.administratorcontact100Client();
+            Task<ServiceReference1.readListResponse> task = ws.readListAsync(0,0,null,null,null,null,null);
+            return "True" ;
+        }
+
+        //GET: api/contact/add
+        [HttpGet]
+        [Route("add")]
+        public string Crear()
+        {
+
+            ServiceReference1.administratorcontact100Client ws = new ServiceReference1.administratorcontact100Client();
+            ws.createAsync("",0,"",0);
+            return "True";
+        }
+    }
+}
---
+++ SA_WebServices/CreateContact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SA_WebServices
+{
+    public class CreateContact
+    {
+        public string name { get; set; }
+        public int catid { get; set; }
+        public string language { get; set; }
+        public int published { get; set; }
+
+    }
+}
